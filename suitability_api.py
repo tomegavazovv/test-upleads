d30441cb76804dc4dd3089a7ad80a7758ec22df0
Tome Gavazov <tomegavazov@gmail.com>
2025-02-04 12:24:08 +0100

    and once again

--- suitability_api.py
+++ suitability_api.py
@@ -130,4 +130,4 @@ async def add_cors_headers(request: Request, call_next):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=30) 
